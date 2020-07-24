332587fe4ac60cf20821ca64e27bee107418d8d1
BishopCode <39637027+BishopCode@users.noreply.github.com>
2020-07-24 17:03:39 +0200

    Add files via upload

---
+++ src/components/SearchForm.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+
+const API_KEY = '9a54738f'
+
+export class SearchForm extends Component {
+    state = {
+        inputMovie: ''
+    }
+
+    _handleChange = (e) => {
+        this.setState({ inputMovie: e.target.value })
+    }
+
+    _handleSubmit = (e) => {
+        // Evitar el comportamiento normal del handleSubmit y poder sobreescribirlo
+        e.preventDefault()
+
+        const { inputMovie } = this.state
+        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${inputMovie}`)
+            .then(res => res.json())
+            .then(results => {
+                const { Search = [], totalResults = "0" } = results
+                console.log({Search, totalResults})
+                this.props.onResults(Search)
+            })
+
+    }
+
+    render() {
+        return (
+            <form onSubmit={this._handleSubmit}>
+                <div className="field has-addons">
+                    <div className="control">
+                        <input
+                            className="input"
+                            onChange={this._handleChange}
+                            type="text"
+                            placeholder="Movie to search..." />
+                    </div>
+                    <div className="control">
+                        <button className="button is-info">
+                            Search
+                    </button>
+                    </div>
+                </div>
+            </form>
+        )
+    }
+}
+
